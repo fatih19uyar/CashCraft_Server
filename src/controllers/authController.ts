63ef0316b86f6434815604ee8aa42bbd4e426816
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-09-06 15:52:29 +0200

    forgotPassword & resetPassword & sendVerificationCodeByEmail & verifyEmailActivationCode added.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,54 +1,179 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import UserModel from '../models/User';
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User";
+import {
+  generateResetCode,
+  generateVerificationCode,
+  sendActivationCodeByEmail,
+  sendResetCodeByEmail,
+} from "../utils/email/mailService";
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key'; // JWT gizli anahtarınız
+const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key"; // JWT gizli anahtarınız
 
 export async function signUp(req: Request, res: Response) {
-    try {
-        const { name, phoneNumber, photo, email, password } = req.body;
+  try {
+    const { name, phoneNumber, photo, email, password } = req.body;
 
-        const existingUser = await UserModel.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Bu e-posta zaten kullanılıyor.' });
-        }
+    // Şifreyi şifreleyin
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await UserModel.create({
-            name,
-            phoneNumber,
-            photo,
-            email,
-            password: hashedPassword,
-        });
-
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '7d' });
-
-        res.status(201).json({ user, token });
-    } catch (error) {
-        res.status(500).json({ message: 'Bir hata oluştu. Lütfen tekrar deneyin.' });
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Bu e-posta zaten kullanılıyor." });
     }
+
+    const user = await UserModel.create({
+      name,
+      phoneNumber,
+      photo,
+      email,
+      password: hashedPassword, // Şifrelenmiş şifreyi kaydedin
+    });
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
 }
 
 export async function signIn(req: Request, res: Response) {
-    try {
-        const { email, password } = req.body;
-
-        const user:any = await UserModel.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: 'Kullanıcı bulunamadı.' });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Geçersiz parola.' });
-        }
-
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '7d' });
-
-        res.status(200).json({ user, token });
-    } catch (error) {
-        res.status(500).json({ message: 'Bir hata oluştu. Lütfen tekrar deneyin.' });
+  try {
+    const { email, password } = req.body;
+    const user: any = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı." });
     }
+    if (!user.isEmailVerified) {
+      return res.status(401).json({ message: "E-posta doğrulanmamış." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Geçersiz parola." });
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
+}
+
+export async function forgotPassword(req: Request, res: Response) {
+  try {
+    const { email } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı." });
+    }
+
+    const resetCode = generateResetCode();
+
+    //await sendResetCodeByEmail(user.email, resetCode); // E-posta sunucu durumu halledilince açılacak
+
+    user.resetCode = resetCode;
+    await user.save();
+
+    return res.status(200).json({ message: "Reset kodu gönderildi." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
+}
+
+export async function resetPassword(req: Request, res: Response) {
+  try {
+    const { email, resetCode, newPassword } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı." });
+    }
+
+    if (user.resetCode !== resetCode) {
+      return res.status(401).json({ message: "Geçersiz reset kodu." });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    user.resetCode = undefined;
+    await user.save();
+
+    return res.status(200).json({ message: "Şifre sıfırlandı." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
+}
+export async function sendVerificationCodeByEmail(req: Request, res: Response) {
+  try {
+    const { email } = req.body;
+    // Kullanıcıyı bulun veya oluşturun
+    let user: any = await UserModel.findOne({ email });
+    if (!user) {
+      user = new UserModel({ email });
+    }
+
+    // E-posta aktivasyon kodunu oluşturun ve kaydedin
+    const verificationCode: any = generateVerificationCode();
+    user.verificationCode = verificationCode;
+    await user.save();
+
+    // E-posta aktivasyon kodunu kullanıcıya gönderin
+    await sendActivationCodeByEmail(user.email, verificationCode);
+
+    return res.status(200).json({ message: "Aktivasyon kodu gönderildi." });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
+}
+
+export async function verifyEmailActivationCode(req: Request, res: Response) {
+  try {
+    const { email, verificationCode } = req.body;
+
+    // Kullanıcıyı bulun
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı." });
+    }
+    // E-posta zaten doğrulandıysa
+    if (user.isEmailVerified) {
+      return res.status(400).json({ message: "E-posta zaten doğrulandı." });
+    }
+    // E-posta aktivasyon kodunu kontrol edin
+    if (user.verificationCode !== verificationCode) {
+      return res.status(401).json({ message: "Geçersiz aktivasyon kodu." });
+    }
+    // E-posta doğrulama başarılı, isEmailVerified'i true olarak güncelleyin
+    user.isEmailVerified = true;
+    await user.save();
+
+    return res.status(200).json({ message: "E-posta doğrulandı." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Bir hata oluştu. Lütfen tekrar deneyin." });
+  }
 }
