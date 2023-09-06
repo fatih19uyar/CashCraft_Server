e0463e51dee73b7128b593542ffc6c894967460b
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-09-06 15:54:55 +0200

    Global exception added. config.sample.ts updated.

--- config.sample.ts
+++ config.sample.ts
@@ -1,15 +1,21 @@
-const config_sample = {
+const config = {
+  httpPort: 4000,
   secretKey: "12345-67890-09876-54321",
   mongoUrl:
-    "mongodb://ontime:ontime123!@c.idvlabs.com:27017/wallet?authSource=admin",
+    "mongodb://ontime:ontime123!@c.idvlabs.com:27017/wallet?authSource=wallet",
   mongo: {
     username: "ontime",
     password: "ontime123!",
     host: "c.idvlabs.com",
     database: "wallet",
-    backupPath: "/home/ubuntu/Backups",
-    port: 4000,
+    port: 27017,
+  },
+  email: {
+    HOST: "smtp.yandex.com",
+    PORT: 587,
+    USERNAME: "test@idvlabs.com",
+    PASSWORD: "ZRBEG6PckJVqU9A",
   },
 };
 
-export default config_sample;
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import config from "../config";
 import UserModel from "./models/User";
 import express from "express";
 import authRoutes from "./routers/authRoutes";
+import { Request, Response, NextFunction } from "express";
 
-const mongoURL = `mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.database}`;
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-mongoose.connect(mongoURL, {
+mongoose.connect(config.mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -19,8 +19,20 @@ mongoose.connect(mongoURL, {
 
 const db = mongoose.connection;
 
-db.on("connected", () => {
+db.once("connected", async () => {
   console.log("Connected to MongoDB successfully.");
+  const collectionsToCheck = ["users", "campaigns", "transactiondata"];
+  for (const collectionName of collectionsToCheck) {
+    const collInfo = await mongoose.connection.db
+      .listCollections({ name: collectionName })
+      .next();
+    if (!collInfo) {
+      await mongoose.connection.db.createCollection(collectionName);
+      console.log(`${collectionName} collection created.`);
+    } else {
+      console.log(`${collectionName} collection already exists.`);
+    }
+  }
 });
 db.on("error", (err) => {
   console.error("MongoDB connection error:", err.message);
@@ -36,7 +48,23 @@ process.on("SIGINT", () => {
     process.exit(0);
   });
 });
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+//app.use("/campaing", campaingRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error: any = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status((error as any).status || 500).json({
+    message: error.message || "Internal Server Error",
+    error: {
+      status: (error as any).status || 500,
+    },
+  });
+});
 
 app.listen(config.httpPort, () => {
   console.log(`Server running on ${config.httpPort} port.`);
